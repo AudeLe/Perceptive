9597f71fa4bab58554cb57f7d8ab38d310a94547
Aude Leissen <aude.leissen@gmail.com>
2022-03-13 13:34:47 +0000

    Retrieving and returning NASA data

--- script.js
+++ script.js
@@ -4,11 +4,30 @@ document.addEventListener('DOMContentLoaded', function(){
     jQuery(document).on('click', '#submitBtn', function(e){
         e.preventDefault();
 
+        var startDate = jQuery('#start_date').val() ? jQuery('#start_date').val() : null;
+        var endDate = jQuery('#end_date').val() ? jQuery('#end_date').val() : null;
+
         jQuery.getJSON('script.php', {
             command: 'fetch',
+            start_date: startDate,
+            end_date: endDate,
         }).done(function(data){
             console.log(data);
-            console.log('WS successful');
+            let results;
+            jQuery.each(data, function(i, item){
+                var result = '<td>' + data[i].neo_reference_id + '</td>';
+                result += '<td>' + data[i].name + '</td>';
+                result += '<td>' + data[i].close_approach_data[0].close_approach_date_full + '</td>';
+
+
+                results += '<tr>' + result + '</tr>';
+            });
+            jQuery('#results').append(results);
+
+            if(data.error){
+                console.log('Error message: ' + data.error);
+            }
+
         }).fail(function(data){
             console.log(data);
             console.log('An error occurred, we could not retrieve the data. Pleas try again later.');
--- script.php
+++ script.php
@@ -4,19 +4,24 @@ $command = $_GET['command'] ?: NULL;
 $url = 'https://api.nasa.gov/neo/rest/v1/feed?';
 
 if($command == 'fetch'){
-    $start_date = $_GET['start_date'] ?: '2022-01-01';
-    $end_date = $_GET['end_date'] ?: '2022-01-08';
-    $api_key = 'DEMO_KEY';
+    $start_date = ($_GET['start_date'] !== NULL) ? $_GET['start_date'] : '2022-01-01';
+    $end_date = ($_GET['end_date'] !== NULL) ? $_GET['end_date'] : '2022-01-08';
+    $api_key = 'sQw5jfJKugnMWHIfKCiWNrtOYGB89eGzR5tpqr4g';
 
     $post = 'start_date=' . $start_date . '&end_date=' . $end_date . '&api_key=' . $api_key . '';
 
-    send_command($url, $post, $response);
+    $url = $url . $post;
 
-    echo $response;
+    send_command($url, $response);
+
+    $results = [];
+    sort_results($response, $results);
+
+    echo json_encode($results);
 
 }
 
-function send_command($url, $post, &$response){
+function send_command($url, &$response){
 
 
     $ch = curl_init();
@@ -27,7 +32,7 @@ function send_command($url, $post, &$response){
         CURLOPT_ENCODING => '',
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => "GET",
-        CURLOPT_POSTFIELDS => $post,
+        CURLOPT_POSTFIELDS => "",
         CURLOPT_HTTPHEADER => [
             "Content-Type: application/json",
             "cache-control: no-cache"
@@ -35,7 +40,31 @@ function send_command($url, $post, &$response){
     ]);
 
     $response = curl_exec($ch);
-    $err = curl_error($ch);
+    $err = curl_error($ch) ?: NULL;
+
 
     return $response;
+}
+
+function sort_results($response, &$results){
+
+    $response = json_decode($response);
+
+    if($response->near_earth_objects){
+        // Loop through the dates
+        foreach($response->near_earth_objects as $items){
+            // Loop through the near earth objects themselves
+            foreach($items as $item){
+                if($item->is_potentially_hazardous_asteroid == TRUE){
+                    $results[] = $item;
+                }
+            }
+
+        }
+    }
+
+    //usort($results, fn($a, $b) => strcmp($a->close_approach_data->epoch_date_close_approach > $b->close_approach_data->epoch_date_close_approach));
+
+
+    return $results;
 }
